a39a2951f14d3d80104becd53c1fe36e0fb864b7
carter <carter@pop-os.localdomain>
2022-03-23 20:46:07 -0600

    updated install script to also install neovim

--- install.sh
+++ install.sh
@@ -1,12 +1,22 @@
+# install neovim
+sudo add-apt-repository ppa:neovim-ppa/stable
+sudo apt-get update -y
+sudo apt-get install neovim -y
+sudo apt-get install python-dev python-pip python3-dev python3-pip
+
+# add  fonts
+mkdir $HOME/.fonts
+mv ./Fira* $HOME/.fonts
+
+# add language servers for vscode, tailwind, and typescript
 npm install -g vscode-langservers-extracted
 npm install -g @tailwindcss/language-server
 npm install -g typescript typescript-language-server
 
-mkdir $HOME/.fonts
-mv ./Fira* $HOME/.fonts
-
+# needed for coq, lsp completion
 sudo apt install python3-venv -y
 
+# packer, manager for neovim
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
